ff4fb8e42a6e978bdb3416720120be5257b4377e
Alexander Wang <alexawang@NVIDIA.com>
2026-06-02 16:28:51 -0700

    chore: updated .gitignore and remove .cache

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -2,9 +2,11 @@
 #include <cstdint>
 
 uint32_t factorial( uint32_t num ) {
-  return num <= 1 ? num : factorial(num-1);
+  return num <= 1 ? 1 : num * factorial(num-1);
 }
 
 TEST_CASE("computed factorials", "[factorial]") {
   REQUIRE(factorial(1) == 1);
+  REQUIRE(factorial(2) == 2);
+  REQUIRE(factorial(10) == 3'628'800);
 }
